36444a2b1f2b1353dcb871ea4457a060f3cdccb2
Shohel <gsshohel1314@gmail.com>
2025-04-28 11:42:16 +0600

    Variables is a name which is set a memory location

--- ApnaCollege/Lecture1.py
+++ ApnaCollege/Lecture1.py
@@ -1,3 +1,10 @@
 # First Program
 print("Hello World")
 print("My name is", "Shohel Rana")
+
+# Variables
+name = "Shohel Rana"
+age = 30
+isStudent = True
+
+print("I am", name, "and i am", age, "years old.")
