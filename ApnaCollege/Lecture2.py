f0c64818f0182e566bb07da0b1fb277439cf4aea
Shohel <gsshohel1314@gmail.com>
2025-04-29 18:38:21 +0600

    Escape sewuence characters

---
+++ ApnaCollege/Lecture2.py
@@ -0,0 +1,11 @@
+# String
+str1 = "This is apna college's tutorial"
+str2 = 'ApnaCollege'
+str3 = """This is a string"""
+
+# Escape sequence characters
+str4 = "This is a string. \nwe are creating it in python"
+print(str4)
+
+str5 = "This is a string. \twe are creating it in python"
+print(str5)
